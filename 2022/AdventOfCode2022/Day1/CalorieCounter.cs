19a26e4a2295e0f762cfb80957d4e91aaf673b84
Oscar Bäck <oscar.back96@outlook.com>
2022-12-01 11:06:55 +0100

    Added 2022 c# proj

---
+++ 2022/AdventOfCode2022/Day1/CalorieCounter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode2022.Day1 {
+	public class CalorieCounter {
+		public List<Elve> ParseElveCalorieData(string data) {
+			string[] dataArr = data.Split("\n");
+			List<Elve> res = ParseElveCalorieDataArr(dataArr);
+			return res;
+		}
+
+		private List<Elve> ParseElveCalorieDataArr(string[] dataArr) {
+			int calorieCount = 0;
+			List<Elve> elves = new List<Elve>();
+			foreach(string str in dataArr) {
+				if(str == "") {
+					elves.Add(new Elve() { Calories = calorieCount });
+					calorieCount = 0;
+				} else
+					calorieCount += int.Parse(str);
+			}
+			elves.Add(new Elve() { Calories = calorieCount });
+			return elves;
+		}
+
+	}
+}
---
+++ 2022/AdventOfCode2022/Day1/Elve.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode2022.Day1 {
+	public class Elve {
+		public int Calories { get; internal set; }
+	}
+}
