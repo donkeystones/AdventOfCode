2690b8edab43ab52c848277610583ab492230253
Oscar Bäck <oscar.back96@outlook.com>
2022-12-11 16:53:41 +0100

    Began day 7 fluff

---
+++ 2022/AdventOfCode2022/Day7/DirectoryManager.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode2022.Day7 {
+    public class DirectoryManager {
+    }
+}
---
+++ 2022/AdventOfCodeTest2022/Day7/DirectoryManagerTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AdventOfCode2022.Day7;
+using NUnit.Framework;
+
+namespace AdventOfCodeTest2022.Day7 {
+	public class DirectoryManagerTest {
+        DirectoryManager dirManager;
+        [SetUp]
+		public void Setup() {
+			dirManager = new DirectoryManager();
+		}
+		[Test]
+		public void CreateDir() {
+			dirManager.
+		}
+	}
+}
