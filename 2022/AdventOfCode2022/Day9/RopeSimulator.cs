ce96e217213ea554cc7d30f559134fb1c4b76647
Oscar Bäck <oscar.back96@outlook.com>
2023-06-03 10:11:10 +0200

    Ropesimulator

---
+++ 2022/AdventOfCode2022/Day9/RopeSimulator.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode2022.Day9 {
+    public class RopeSimulator {
+
+    }
+}
---
+++ 2022/AdventOfCodeTest2022/Day9/RopeSimulatorTest.cs
@@ -0,0 +1,19 @@
+﻿using AdventOfCode2022.Day9;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCodeTest2022.Day9 {
+    public class RopeSimulatorTest {
+        RopeSimulator ropeSimulator;
+        [SetUp]
+        public void Setup() {
+            ropeSimulator = new RopeSimulator();
+        }
+        [Test]
+        public void StartingPositionHeadAndTail() {
+
+        }
+    }
+}
